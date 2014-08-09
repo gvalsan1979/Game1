6adbfa6c20240162fd0a1a1ba07810e9c9541984
Shine Wang <shinewang@live.ca>
2014-08-09 15:34:15 -0400

    Added "princess" and "enemies" (sprites notwithstanding)

---
+++ ChildrenRehabilitationGame/Assets/Scripts/EnemySpawnerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerBehaviour : MonoBehaviour {
+    public GameObject enemy;
+    private const int FPS = 30;
+    public static int NumEnemies = 1;
+    private int m_frameCount;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        m_frameCount++;
+        if (NumEnemies <= 10 && m_frameCount % FPS == 0) {
+            Instantiate(enemy, new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f)), new Quaternion());
+            NumEnemies++;
+        }
+    }
+}
